d12014c1e92f08f887ca0cab7c7a20a15c1fc44b
Thomas Bouder <tbouder@protonmail.com>
2019-09-26 12:58:27 +0200

    Add error 498 & 499

--- Codes.go
+++ Codes.go
@@ -5,7 +5,7 @@
 ** @Filename:				codes.go
 **
 ** @Last modified by:		Tbouder
-** @Last modified time:		Thursday 26 September 2019 - 12:52:34
+** @Last modified time:		Thursday 26 September 2019 - 12:58:11
 *******************************************************************************/
 
 package		errors
@@ -15,5 +15,7 @@ const CLIENT_401 = `You are not allowed to access this ressource`
 const CLIENT_403 = `You are not allowed to access this ressource`
 const CLIENT_404 = `One or many elements were not found`
 const CLIENT_409 = `You are facing a conflict. Please verify what you want and the existing data`
+const CLIENT_498 = `Your token is invalid`
+const CLIENT_499 = `You need a token to access this ressource`
 
 const SERV_500 = `Unexpected error`
