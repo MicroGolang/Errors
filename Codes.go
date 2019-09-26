9b8c6554a0b549f21d72bef92fa9878673a58afa
Thomas Bouder <tbouder@protonmail.com>
2019-09-26 12:52:40 +0200

    error to errors

--- Codes.go
+++ Codes.go
@@ -5,7 +5,7 @@
 ** @Filename:				codes.go
 **
 ** @Last modified by:		Tbouder
-** @Last modified time:		Thursday 26 September 2019 - 12:45:09
+** @Last modified time:		Thursday 26 September 2019 - 12:52:34
 *******************************************************************************/
 
 package		errors
@@ -15,3 +15,5 @@ const CLIENT_401 = `You are not allowed to access this ressource`
 const CLIENT_403 = `You are not allowed to access this ressource`
 const CLIENT_404 = `One or many elements were not found`
 const CLIENT_409 = `You are facing a conflict. Please verify what you want and the existing data`
+
+const SERV_500 = `Unexpected error`
